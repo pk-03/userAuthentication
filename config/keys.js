794de256fc6fcba44563a146131bea474004bac3
thorgithub <46312006+thorgithub@users.noreply.github.com>
2019-01-09 15:39:39 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://Pranit:Pranit1998@cluster0-yttwx.mongodb.net/test?retryWrites=true'
+    mongoURI : 'mongodb+srv://<>@cluster0-yttwx.mongodb.net/test?retryWrites=true'
 
 }
